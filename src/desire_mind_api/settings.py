207cb9c41438b27d73fc74879467ded3649e0ce8
Vlad Rusu <vlad.rusu@linnify.com>
2020-04-26 12:54:43 +0300

    fixed run.sh, change database to postgres

--- src/desire_mind_api/settings.py
+++ src/desire_mind_api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'desire_mind_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_DB'),
+        'USER': os.environ.get('DATABASE_USERNAME'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST': os.environ.get('DATABASE_HOST'),
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
