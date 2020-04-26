444fec79fc2472678758dc262fbcaba750f52e5c
Vlad Rusu <vlad.rusu@linnify.com>
2020-04-26 13:25:28 +0300

    fix urls, remove pycache, add no pycache to env file

--- src/wishes/urls.py
+++ src/wishes/urls.py
@@ -6,6 +6,6 @@ from . import views
 
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register("wishes", views.Wish, base_name='wishes')
+router.register("wishes", views.WishViewModel, base_name='wishes')
 
 urlpatterns = [url(r"^", include(router.urls))]
